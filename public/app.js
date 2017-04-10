4ef67f85e015e1886e73c7ffa2da495cb088da5d
Suzanne <suzanne.aitchison@gmail.com>
2017-04-10 16:08:02 +0100

    refactored into separate functions

--- public/app.js
+++ public/app.js
@@ -1,26 +1,47 @@
-  var addCat = function(name, faveFood, imgSource){
+  var createNewCatList = function(){
     var list = document.createElement('ul')
     list.classList.add('cat')
+    return list
+  }
 
+  var createCatNameLine = function(name){
     var nameLine = document.createElement('li')
     nameLine.innerText = name
+    return nameLine
+  }
 
+  var createCatFoodLine = function(faveFood){
     var foodLine = document.createElement('li')
     foodLine.innerText = faveFood
+    return foodLine
+  }
 
+  var addCatImageLine = function(imgSource){
     var image = document.createElement('img')
     image.width = '500'
     image.src = imgSource
 
     var imageLine = document.createElement('li')
     imageLine.appendChild(image)
+    return imageLine
+  }
 
+  var appendCatElements = function(list, nameLine, foodLine, imageLine){
     list.appendChild(nameLine)
     list.appendChild(foodLine)
     list.appendChild(imageLine)
 
     var catsSection = document.querySelector('#cats')
-    catsSection.appendChild(list)
+    catsSection.appendChild(list)    
+  }
+
+  var addCat = function(name, faveFood, imgSource){
+    var list = createNewCatList()
+    var nameLine = createCatNameLine(name)
+    var foodLine = createCatFoodLine(faveFood)
+    var imageLine = addCatImageLine(imgSource)
+    
+    appendCatElements(list, nameLine, foodLine, imageLine)
   }
 
   app = function(){
